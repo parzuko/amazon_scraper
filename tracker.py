53bc8e81e05a39dd19aeeafae7c05e40d2544b7c
parzuko <jivansh17@gmail.com>
2020-07-19 10:39:25 +0530

    Refactor Code

--- tracker.py
+++ tracker.py
@@ -34,8 +34,9 @@ class AmazonAPI():
         print(f"Looking for {self.search_term} products...")
         links = self.get_products_links()
         time.sleep(3)
-
+        #products = self.get_products_links(links)
         self.driver.quit()
+
 
     def get_products_links(self):
         self.driver.get(self.base_url)
@@ -43,8 +44,20 @@ class AmazonAPI():
         element.send_keys(self.search_term)
         element.send_keys(Keys.ENTER)
         time.sleep(2)
+        self.driver.get(f'{self.driver.current_url}{self.price_filter}')
+        time.sleep(2)
+        result_list = self.driver.find_element_by_class_name('s-result-list')
 
-
+        links = []
+        try:
+            results  = result_list[0].find_element_by_xpath(
+                "//div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]")
+            links = [link.get_attribute('href') for link in results]
+            return links
+        except Exception as e:
+            print("Didnt get anything...")
+            print(e)
+            return links
 
 if __name__ == '__main__':
     print("HELo")
